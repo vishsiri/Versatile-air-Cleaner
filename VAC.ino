dae3d69039c5ac2a9805e0961188104b1daf1852
vishsiri <s.vish555@gmail.com>
2021-02-13 09:38:02 +0700

    Update VAC.ino

--- VAC.ino
+++ VAC.ino
@@ -1,8 +1,23 @@
-void setup() {
-  
+#include <Blynk.h>
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#define ON HIGH
+#define OFF LOW
+char auth[] = ""; //รหัส Token Blynk
+char ssid[] = ""; //ชื่อ wifi
+char pass[] = ""; //รหัส wifi
+
+void setup()
+{
+
+Serial.begin(9600);
+Blynk.begin(auth, ssid, pass);
+
 }
 
-void loop() {
-  
-
+void loop()
+{
+Blynk.run();
 }
